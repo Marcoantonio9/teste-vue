7425beb753e2b45872b0196a287db513cb0fc7f0
Marcoantonio9 <marcojr918@gmail.com>
2021-08-18 13:47:20 -0300

    Adicionando componentes

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Header />
+    <Form />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Form from './components/Form.vue'
+export default {
+  components: {Header, Form}
+
+}
+</script>
+
+<style>
+*{
+  padding: 0px;
+  margin: 0px;
+  box-sizing: border-box;
+}
+</style>

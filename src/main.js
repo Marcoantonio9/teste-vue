e2ed1c0c10c3dfc47bc125ee2350373aa42f2ad7
Marcoantonio9 <marcojr918@gmail.com>
2021-08-18 20:40:24 -0300

    Configurando Vue router e V-mask

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
@@ -12,8 +13,21 @@ Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
 
+// As a plugin
+import VueMask from 'v-mask'
+Vue.use(VueMask);
+
+// Or as a directive
+import { VueMaskDirective } from 'v-mask'
+Vue.directive('mask', VueMaskDirective);
+
+// Or only as a filter
+import { VueMaskFilter } from 'v-mask'
+Vue.filter('VMask', VueMaskFilter)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
 }).$mount('#app')

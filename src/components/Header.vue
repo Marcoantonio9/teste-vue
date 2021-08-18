361416e6f9f5070a9d51c709288cc173197c49a7
Marcoantonio9 <marcojr918@gmail.com>
2021-08-18 13:47:34 -0300

    Estilizando navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <b-navbar toggleable type="dark" variant="dark" class="px-5 pt-3 pb-3">
+    <b-navbar-brand href="#">Teste Front-End</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

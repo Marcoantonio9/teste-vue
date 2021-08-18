f5bcf74c6e45a65fc5da3b2d755c58afc3d68eed
Marcoantonio9 <marcojr918@gmail.com>
2021-08-18 20:41:20 -0300

    Novas funções header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,34 @@
 <template>
   <b-navbar toggleable type="dark" variant="dark" class="px-5 pt-3 pb-3">
     <b-navbar-brand href="#">Teste Front-End</b-navbar-brand>
+    <b-button 
+      v-if="this.$route.name === 'ListaUsuarios'" 
+      @click="voltar"
+      variant="outline-light"
+      >
+      <i class="fas fa-arrow-left"></i> 
+      Voltar
+    </b-button>
   </b-navbar>
 </template>
 
 <script>
 export default {
+  data(){
+    return{
+      nomeRotas: ''
+    }
+  },
+  methods: {
+    voltar(){
 
+      this.$router.push({path: '/'})
+    }
+  },
+  created(){
+
+    console.log(this.nomeRotas)
+  }
 }
 </script>
 
